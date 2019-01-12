01dd2d38c2a4134f645fabe9d0f2c611017a3459
CircleCI <circleci@oktw.network>
2019-01-12 11:36:49 +0000

    Fix animation for title

---
+++ en/assets/js/main.js
@@ -0,0 +1,13 @@
+
+function setFixedSize(element){
+    element.height(element.height());
+    element.width(element.width());
+}
+
+function setWidth(element,width){
+    element.width(width);
+}
+
+function setHeight(element,height){
+    element.height(height);
+}
