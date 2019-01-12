32f42b4ad45cc8298821322095cced657fd5d672
Sciuridae0603 <Sciuridae0603@gmail.com>
2019-01-12 16:36:37 +0800

    Fix CircleCI

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,15 +1,17 @@
 git config user.name "CircleCI"
 git config user.email "circleci@oktw.network"
 
-find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+mv _site ./../
+mv .git ./../
 
-pwd
+rm -rf ./*
+cp -R ./../.git ./
 
 git checkout gh-pages
-git pull origin gh-pages
 
-mv _site/* .
-rm -R _site/
+find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+
+cp -R ./../_site/* ./
 
 git add -fA
 git commit --allow-empty -m "$(git log master -1 --pretty=%B)"
